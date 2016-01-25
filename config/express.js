0d5d78db3f5ce835f0779f99c76cd9793122637c
Kunal Kapadia <kunalkapadia12@gmail.com>
2016-01-26 01:53:18 +0530

    Add winston logging library. Log each API request and error.

--- config/express.js
+++ config/express.js
@@ -6,6 +6,8 @@ import compress from 'compression';
 import methodOverride from 'method-override';
 import cors from 'cors';
 import httpStatus from 'http-status';
+import expressWinston from 'express-winston';
+import winstonInstance from './winston';
 import routes from '../server/routes';
 import config from './env';
 import * as utilityService from '../server/services/utility';
@@ -25,6 +27,18 @@ app.use(methodOverride());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
+// enable detailed API logging in dev env
+if (config.NODE_ENV === 'development') {
+	expressWinston.requestWhitelist.push('body');
+	expressWinston.responseWhitelist.push('body');
+	app.use(expressWinston.logger({
+		winstonInstance,
+		meta: true, 	// optional: log meta data about request (defaults to true)
+		msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+		colorStatus: true 	// Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+	}));
+}
+
 // mount all routes on /api path
 app.use('/api', routes);
 
@@ -40,11 +54,16 @@ app.use((req, res, next) => {
 	return next(err);
 });
 
+// log error in winston transports
+app.use(expressWinston.errorLogger({
+	winstonInstance
+}));
+
 // error handler, send stacktrace only during development
 app.use((err, req, res, next) =>		// eslint-disable-line no-unused-vars
 	res.status(err.status).json({
 		message: err.isPublic ? err.message : httpStatus[err.status],
-		error: config.NODE_ENV === 'development' ? err : {}
+		stack: config.NODE_ENV === 'development' ? err.stack : {}
 	})
 );
 
