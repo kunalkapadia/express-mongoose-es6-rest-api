1e6fd9c6efb024b43c5a44f1739cbdc146414b0f
Kunal Kapadia <kunalkapadia12@gmail.com>
2016-08-20 20:45:17 +0530

    feat(JWT): Add support for JWT Authentication (#29)
    Closes #27

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,6 @@
 export default {
   env: 'development',
+  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
   db: 'mongodb://localhost/express-mongoose-es6-rest-api-development',
   port: 3000
 };
