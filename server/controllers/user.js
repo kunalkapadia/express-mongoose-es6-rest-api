abfc2b0d34350e0266f2c29e7cfe36192ca34bb7
Kunal Kapadia <kunalkapadia12@gmail.com>
2016-09-23 21:29:25 +0530

    pdate eslint to v3.5. Fix lint errors. Move from gulp to npm task for lint. Closes #44, #45.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,10 +6,10 @@ import User from '../models/user';
 function load(req, res, next, id) {
   User.get(id)
     .then((user) => {
-      req.user = user;		// eslint-disable-line no-param-reassign
+      req.user = user; // eslint-disable-line no-param-reassign
       return next();
     })
-    .catch((e) => next(e));
+    .catch(e => next(e));
 }
 
 /**
@@ -33,8 +33,8 @@ function create(req, res, next) {
   });
 
   user.save()
-    .then((savedUser) => res.json(savedUser))
-    .catch((e) => next(e));
+    .then(savedUser => res.json(savedUser))
+    .catch(e => next(e));
 }
 
 /**
@@ -49,8 +49,8 @@ function update(req, res, next) {
   user.mobileNumber = req.body.mobileNumber;
 
   user.save()
-    .then((savedUser) => res.json(savedUser))
-    .catch((e) => next(e));
+    .then(savedUser => res.json(savedUser))
+    .catch(e => next(e));
 }
 
 /**
@@ -62,8 +62,8 @@ function update(req, res, next) {
 function list(req, res, next) {
   const { limit = 50, skip = 0 } = req.query;
   User.list({ limit, skip })
-    .then((users) =>	res.json(users))
-    .catch((e) => next(e));
+    .then(users => res.json(users))
+    .catch(e => next(e));
 }
 
 /**
@@ -73,8 +73,8 @@ function list(req, res, next) {
 function remove(req, res, next) {
   const user = req.user;
   user.remove()
-    .then((deletedUser) => res.json(deletedUser))
-    .catch((e) => next(e));
+    .then(deletedUser => res.json(deletedUser))
+    .catch(e => next(e));
 }
 
 export default { load, get, create, update, list, remove };
--- server/tests/misc.test.js
+++ server/tests/misc.test.js
@@ -1,7 +1,6 @@
 import request from 'supertest-as-promised';
 import httpStatus from 'http-status';
-import chai from 'chai';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import app from '../../index';
 
 chai.config.includeStack = true;
@@ -12,10 +11,11 @@ describe('## Misc', () => {
       request(app)
         .get('/api/health-check')
         .expect(httpStatus.OK)
-        .then(res => {
+        .then((res) => {
           expect(res.text).to.equal('OK');
           done();
-        });
+        })
+        .catch(done);
     });
   });
 
@@ -24,10 +24,11 @@ describe('## Misc', () => {
       request(app)
         .get('/api/404')
         .expect(httpStatus.NOT_FOUND)
-        .then(res => {
+        .then((res) => {
           expect(res.body.message).to.equal('Not Found');
           done();
-        });
+        })
+        .catch(done);
     });
   });
 
@@ -36,10 +37,11 @@ describe('## Misc', () => {
       request(app)
         .get('/api/users/56z787zzz67fc')
         .expect(httpStatus.INTERNAL_SERVER_ERROR)
-        .then(res => {
+        .then((res) => {
           expect(res.body.message).to.equal('Internal Server Error');
           done();
-        });
+        })
+        .catch(done);
     });
 
     it('should handle express validation error - username is required', (done) => {
@@ -49,10 +51,11 @@ describe('## Misc', () => {
           mobileNumber: '1234567890'
         })
         .expect(httpStatus.BAD_REQUEST)
-        .then(res => {
-          expect(res.body.message).to.equal(`"username" is required`);
+        .then((res) => {
+          expect(res.body.message).to.equal('"username" is required');
           done();
-        });
+        })
+        .catch(done);
     });
   });
 });
