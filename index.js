e27292631da0d61f1ee97293ca0d962495d0104e
Kunal Kapadia <kunalkapadia12@gmail.com>
2016-09-23 19:49:12 +0530

    Instead of promisifying all methods of mongoose, plugin bluebird into… (#39)
    * Instead of promisifying all methods of mongoose, plugin bluebird into mongoose
    
    * Update node version in travis config

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 import config from './config/env';
 import app from './config/express';
 
-// promisify mongoose
-Promise.promisifyAll(mongoose);
+// plugin bluebird promise in mongoose
+mongoose.Promise = Promise;
 
 // connect to mongo db
 mongoose.connect(config.db, { server: { socketOptions: { keepAlive: 1 } } });
