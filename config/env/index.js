0c213d103a28592360d0de56ca85fd2593aeb879
Kunal Kapadia <kunalkapadia12@gmail.com>
2016-09-30 00:48:38 +0530

    Update eslint and other dependencies

--- config/env/index.js
+++ config/env/index.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(`./${env}`);
+const config = require(`./${env}`); // eslint-disable-line import/no-dynamic-require
 
 const defaults = {
   root: path.join(__dirname, '/..')
