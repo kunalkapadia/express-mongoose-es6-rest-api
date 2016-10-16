73a245e4e7162c5a2b8d33e23525acf7f2fd4a86
Kunal Kapadia <kunalkapadia12@gmail.com>
2016-10-17 00:23:35 +0530

    Update express-jwt and move port to 4040

--- config/env/test.js
+++ config/env/test.js
@@ -2,5 +2,5 @@ export default {
   env: 'test',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
   db: 'mongodb://localhost/express-mongoose-es6-rest-api-test',
-  port: 3000
+  port: 4040
 };
