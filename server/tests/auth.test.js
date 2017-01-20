d3f73e49902b758b4feb38013bf5b0be97ba0879
Oliver Sahner <osahner@medianet.de>
2017-01-20 13:49:09 +0100

    verify JWT token

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,7 +1,11 @@
 import request from 'supertest-as-promised';
 import httpStatus from 'http-status';
+import jwt from 'jsonwebtoken';
 import chai, { expect } from 'chai';
 import app from '../../index';
+
+const config = require('../../config/env');
+const should = require('chai').should();
 
 chai.config.includeStack = true;
 
@@ -12,16 +16,20 @@ describe('## AUTH APIs', () => {
   };
   let jwtToken;
 
-  describe('# GET /api/auth/login (login to get JWT token)', () => {
-    it('should get JWT token', (done) => {
+  describe('# GET /api/auth/login', () => {
+    it('should get (valid) JWT token', (done) => {
       request(app)
         .post('/api/auth/login')
         .send(user)
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body.username).to.equal(user.username);
-          jwtToken = `Bearer ${res.body.token}`;
-          done();
+          should.exist(res.body.token);
+          jwt.verify(res.body.token, config.jwtSecret, (err, decoded) => {
+            should.not.exist(err);
+            expect(decoded.username).to.equal(user.username);
+            jwtToken = `Bearer ${res.body.token}`;
+            done();
+          });
         })
         .catch(done);
     });
