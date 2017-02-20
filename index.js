c1e00549ea647aa78bbd48c3483d239ecc40af26
Kunal Kapadia <kunalkapadia12@gmail.com>
2017-02-20 20:26:05 +0530

    Use dotenv for configuration management (#302)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import util from 'util';
-import config from './config/env';
+
+// config should be imported before importing any other file
+import config from './config/config';
 import app from './config/express';
 
 const debug = require('debug')('express-mongoose-es6-rest-api:index');
@@ -12,7 +14,8 @@ Promise = require('bluebird'); // eslint-disable-line no-global-assign
 mongoose.Promise = Promise;
 
 // connect to mongo db
-mongoose.connect(config.db, { server: { socketOptions: { keepAlive: 1 } } });
+const mongoUri = `${config.mongo.host}:${config.mongo.port}`;
+mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.db}`);
 });
