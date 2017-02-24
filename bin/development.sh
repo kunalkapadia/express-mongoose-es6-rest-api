5fb0d82ff024ec94aad997887a425a04f23f22c9
Kunal Kapadia <kunalkapadia12@gmail.com>
2017-02-24 21:30:40 +0530

    Update Docker-Compose files as per .env changes

--- bin/development.sh
+++ bin/development.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# --abort-on-container-exit  Stops all containers if any container was stopped.
-docker-compose up --build --abort-on-container-exit
+# --build:  Build images before starting containers.
+docker-compose up --build
