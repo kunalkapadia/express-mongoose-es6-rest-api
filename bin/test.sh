6f96c7ddb1ccea4c69a6550802ab60b26805f052
Kunal Kapadia <kunalkapadia12@gmail.com>
2017-04-05 00:04:35 +0530

    Add docker-compose test

--- bin/test.sh
+++ bin/test.sh
@@ -2,4 +2,5 @@
 
 # --build: Build images before starting containers.
 # --abort-on-container-exit: Stops all containers if any container is stopped
-docker-compose up --build --abort-on-container-exit
+docker-compose -f 'docker-compose.test.yml' -p ci up --build --abort-on-container-exit
+exit $(docker wait ci_express-mongoose-es6-rest-api_1)
