593ca9598fa7b69dc761a1f8ef5d29fdd237bdc0
Surabhi Sharma <surabhisharma.226005@gmail.com>
2017-07-27 12:23:23 +0530

    Removing detached head error

--- travis.sh
+++ travis.sh
@@ -1,11 +1,11 @@
-git config user.name "Surabhi Sharma"
-git config user.email "surabhisharma.226005@gmail.com"
+# git config user.name "Surabhi Sharma"
+# git config user.email "surabhisharma.226005@gmail.com"
 
-#git remote add upstream "https://$GH_TOKEN@github.com/rust-lang/rust-by-example.git"
-git remote add upstream "https://github.com/surabhi226005/express-mongoose-es6-rest-api.git"
-echo "Remote branch added"
-git fetch
-echo "Remote fetched"
-git branch
-git checkout develop
-git pull
+# #git remote add upstream "https://$GH_TOKEN@github.com/rust-lang/rust-by-example.git"
+# git remote add upstream "https://github.com/surabhi226005/express-mongoose-es6-rest-api.git"
+# echo "Remote branch added"
+# git fetch
+# echo "Remote fetched"
+# git branch
+# git checkout develop
+# git pull
