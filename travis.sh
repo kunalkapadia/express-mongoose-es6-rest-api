2b7bcd7401150ceb18758bfe26a3565cf34dd107
Surabhi Sharma <surabhisharma.226005@gmail.com>
2017-07-27 13:27:50 +0530

    Feature/ss/test ci (#12)
    * test 2
    
    * Removing detached head error
    
    * test 3

--- travis.sh
+++ travis.sh
@@ -3,10 +3,10 @@ git config user.email "surabhisharma.226005@gmail.com"
 
 #git remote add upstream "https://$GH_TOKEN@github.com/rust-lang/rust-by-example.git"
 # git remote add upstream "https://github.com/surabhi226005/express-mongoose-es6-rest-api.git"
-echo "Remote branch added"
-git fetch
-echo "Remote fetched"
-git branch --track develop origin/develop
-git branch
+# echo "Remote branch added"
+# git fetch
+# echo "Remote fetched"
+# git branch --track develop origin/develop
+# git branch
 git checkout develop
 git pull
