c4d9b0afdc8db0c656d717e8ef2d6307b2e0d2c0
Kunal Kapadia <kunalkapadia12@gmail.com>
2017-07-28 16:14:04 +0530

    Organise files around components instead of roles. Inspired from https://blog.risingstack.com/node-hero-node-js-project-structure-tutorial/

--- server/auth/auth.route.js
+++ server/auth/auth.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import validate from 'express-validation';
 import expressJwt from 'express-jwt';
 import paramValidation from '../../config/param-validation';
-import authCtrl from '../controllers/auth.controller';
+import authCtrl from './auth.controller';
 import config from '../../config/config';
 
 const router = express.Router(); // eslint-disable-line new-cap
