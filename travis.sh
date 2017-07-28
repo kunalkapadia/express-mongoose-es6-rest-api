2152ca9fecc19c82fecd357c2035a78e8b90b63b
Surabhi Sharma <surabhisharma.226005@gmail.com>
2017-07-28 20:18:03 +0530

    Integrate heroku in travis setup

--- travis.sh
+++ travis.sh
@@ -8,12 +8,29 @@
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email $GIT_USER_EMAIL
 
+# Install the Heroku gem (or the Heroku toolbelt)
+gem install $PROVIDER
+# Add your Heroku git repo:
 git remote rm $PROVIDER ; 
 git remote add $PROVIDER git@$PROVIDER.com:$PROVIDER_APP_NAME.git ;
+# Add your Heroku API key:
+export HEROKU_API_KEY=$HEROKU_API_KEY
+# Turn off warnings about SSH keys:
+echo "   Host heroku.com" >> ~/.ssh/config
+echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+echo "   CheckHostIP no" >> ~/.ssh/config
+echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+# Clear your current Heroku SSH keys:
+$PROVIDER keys:clear
+# Add a new SSH key to Heroku
+yes | $PROVIDER keys:add
 
 git add dist/ ;
-git checkout -b $TRAVIS_BRANCH ;
+git checkout -b $NEW_TRAVIS_BRANCH ;
 git commit -m "$TRAVIS_COMMIT_MESSAGE" ;
-git push $PROVIDER $TRAVIS_BRANCH:PROVIDER_BRANCH ;
+# Push to Heroku!
+git fetch --all --unshallow
+yes | git push --force $PROVIDER $NEW_TRAVIS_BRANCH:$PROVIDER_BRANCH ;
 
-echo -e "yes" | ./travis.sh ; fi
+
+# echo -e "yes" | ./travis.sh
